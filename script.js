33f3e974d82509e8fb98fff94bd086879b0bde2a
larissamartinezpereira-cyber <larissa.martinez.pereira@escola.pr.gov.br>
2026-07-30 10:55:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const botaoTodos = document.querySelector("nav button");
   if (botaoTodos) botaoTodos.classList.add("ativo");
 });
+const btnCurtir = document.getElementById("btnCurtir");
+
+const curtiu = localStorage.getItem("curtiu");
+
+if(curtiu){
+    btnCurtir.classList.add("curtido");
+    btnCurtir.innerHTML = "❤️ Curtido";
+}
+
+btnCurtir.addEventListener("click", () => {
+    if(!btnCurtir.classList.contains("curtido")){
+        btnCurtir.classList.add("curtido");
+        btnCurtir.innerHTML = "❤️ Curtido";
+        localStorage.setItem("curtiu", "true");
+    }else{
+        btnCurtir.classList.remove("curtido");
+        btnCurtir.innerHTML = "🤍 Curtir";
+        localStorage.removeItem("curtiu");
+    }
+});
